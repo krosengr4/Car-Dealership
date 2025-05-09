31e81de7acd5e693bcfd772eff1d2f241a3effa8
krosengr4 <rosenkev4@gmail.com>
2025-05-09 13:24:28 -0500

    Set up work environment for car dealership workshop

---
+++ CarDealership/src/main/java/CarDealershipMain.java
@@ -0,0 +1,11 @@
+import com.pluralsight.*;
+
+public class CarDealershipMain {
+
+    public static void main(String[] args) {
+        System.out.println("\n\t\t___________WELCOME_TO_CODEO_CARS" + Symbols.cowboy + "___________");
+        Design.designLine(70, true);
+
+    }
+
+}
