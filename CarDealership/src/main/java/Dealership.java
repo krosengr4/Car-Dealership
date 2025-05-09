b6acfa0e2dc3ad43dbcf552b66b41ee716d06d0d
krosengr4 <rosenkev4@gmail.com>
2025-05-09 13:49:03 -0500

    Created constructor for dealership and add methods to get vehicles based on vehicle objects in Dealership.java class

--- CarDealership/src/main/java/Dealership.java
+++ CarDealership/src/main/java/Dealership.java
@@ -1,2 +1,54 @@
+import java.util.ArrayList;
+
 public class Dealership {
+
+    String name;
+    String address;
+    String phoneNumber;
+    ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        inventory = new ArrayList<>();
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(int min, int max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehicleByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear (int min, int max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor (String color) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String type) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return new ArrayList<>();
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+
+    }
+
+
 }
